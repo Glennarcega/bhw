7afd843c74ac07a13e7bb6e6e68b8c4911c9da62
KirtSywin <kirtfab@gmail.com>
2023-07-19 10:28:04 +0800

    updated graphs

--- admin/homemed.php
+++ admin/homemed.php
@@ -1,162 +1,125 @@
-
-<?php
-     
-     $dataPoints = array(
-        array("productName"=> "label", "y"),
-     );
-     $link=mysqli_connect("localhost","root","");
-     mysqli_select_db($link,"my_db");
-     $test=array();
- 
-     $count=0;
-    
-     $res=mysqli_query($link,"select * from medicines");
-     while($row=mysqli_fetch_array($res))
-     {
-        $test[$count]["label"]=$row["productName"];
-         $test[$count]["y"]=$row["total"];
-         $count=$count+1;
-     }
-    
-     ?>
-	 <!--  end condtion graph med  -->
-
-     <!--  start condtion graph address  -->
-	 <?php
-     
-     $dataPoints = array(
-        array("address"=> "label", "y"),
-       
-     );
-     $link=mysqli_connect("localhost","root","");
-     mysqli_select_db($link,"my_db");
-     $test=array();
- 
-     $count=0;
-    
-     $res=mysqli_query($link,"select * from residentrecords");
-     while($row=mysqli_fetch_array($res))
-     {
-        $test[$count]["label"]=$row["address"];
-         $test[$count]["y"]=$row["quantity_req"];
-         $count=$count+1;
-     }
-    
-     ?>
-     <!--  start condtion graph address  -->
-
-
-
 <!DOCTYPE html>
 <?php
 	require_once '../admin_query/validate.php';
 	require '../admin_query/name.php';
-?>
-<html lang = "en">
-	<head>
-		<title>Barangay Health Worker</title>
-		<meta charset = "utf-8" />
-		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
-		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
-		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
-	
-       
 
-	</head>
+	// Fetch data for the medicine graph
+	$link = mysqli_connect("localhost", "root", "");
+	mysqli_select_db($link, "my_db");
+	$medicines = array();
+
+	$res = mysqli_query($link, "SELECT * FROM medicines");
+	while ($row = mysqli_fetch_array($res)) {
+		$medicine["label"] = $row["productName"];
+		$medicine["y"] = $row["total"];
+		$medicines[] = $medicine;
+	}
+
+	// Fetch data for the address graph
+	$addresses = array();
+
+	$res = mysqli_query($link, "SELECT * FROM residentrecords");
+	while ($row = mysqli_fetch_array($res)) {
+		$address["label"] = $row["address"];
+		$address["y"] = $row["quantity_req"];
+		$addresses[] = $address;
+	}
+?>
+
+<html lang="en">
+<head>
+	<title>Barangay Health Worker</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="../css/style.css" />
+</head>
+
 <body>
-	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
-		<div  class = "container-fluid">
-			<div class = "navbar-header">
-				<a class = "navbar-brand" >Barangay Health Worker</a>
+	<nav style="background-color: rgba(0, 0, 0, 0.1);" class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand">Barangay Health Worker</a>
 			</div>
-			<ul class = "nav navbar-nav pull-right ">
-				<li class = "dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></i> <?php echo $name;?></a>
+			<ul class="nav navbar-nav pull-right ">
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+						<?php echo $name; ?>
+					</a>
 				</li>
-				<li><a href = "../logout.php">Logout</a></li>	
+				<li><a href="../logout.php">Logout</a></li>
 			</ul>
 		</div>
 	</nav>
-	<div class = "container-fluid">	
-		<ul class = "nav nav-pills">
-			<li class = "active"><a href = "homemed.php">Home</a></li>
-			<li class = ""><a href = "medicine.php">Medicine</a></li>	
-			<li class = ""><a href = "userRecMed.php">Records</a></li>			
-					
-		</ul>	
-	</div>
-	<br />
-	<div class = "container-fluid">
-		<div class = "panel panel-default">
-		
 
-		</div>
+	<div class="container-fluid">
+		<ul class="nav nav-pills">
+			<li class="active"><a href="homemed.php">Home</a></li>
+			<li class=""><a href="medicine.php">Medicine</a></li>
+			<li class=""><a href="userRecMed.php">Records</a></li>
+		</ul>
+	</div>
+	<br />
+	<div class="container-fluid">
+		<div class="panel panel-default"></div>
 	</div>
 	<br />
 	<br />
-<!-- medicine graph -->
+	
+
+	<!-- Medicine graph -->
+	<div id="medicineChartContainer" style="height: 370px; width: 100%;"></div>
+	<br>
+	<br />
+	<br />
+	<!-- Address graph -->
+	<div id="addressChartContainer" style="height: 370px; width: 100%;"></div>
+
+	<!-- Include the CanvasJS library -->
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+	<!-- Render the graphs -->
 	<script>
-		
 		window.onload = function () {
-		 
-		var chart = new CanvasJS.Chart("chartContainer", {
-			animationEnabled: true,
-			theme: "light3", // "light1", "light2", "dark1", "dark2"
-			title: {
-				text: "Medicine"
-			},
-			axisY: {
-				title: "Number of Medicine"
-			},
-			axisX: {
-				title: "Product Name"
-			},
-			data: [{
-				type: "column",
-				dataPoints: <?php echo json_encode($test, JSON_NUMERIC_CHECK); ?>
-			}]
-		});
-		chart.render();
-		 
+			var medicineChart = new CanvasJS.Chart("medicineChartContainer", {
+				animationEnabled: true,
+				theme: "light3",
+				title: {
+					text: "Medicine"
+				},
+				axisY: {
+					title: "Number of Medicine"
+				},
+				axisX: {
+					title: "Product Name"
+				},
+				data: [{
+					type: "column",
+					dataPoints: <?php echo json_encode($medicines, JSON_NUMERIC_CHECK); ?>
+				}]
+			});
+
+			medicineChart.render();
+
+			var addressChart = new CanvasJS.Chart("addressChartContainer", {
+				animationEnabled: true,
+				theme: "light3",
+				title: {
+					text: "Address"
+				},
+				axisY: {
+					title: "Quantity Request"
+				},
+				axisX: {
+					title: "Sitio"
+				},
+				data: [{
+					type: "column",
+					dataPoints: <?php echo json_encode($addresses, JSON_NUMERIC_CHECK); ?>
+				}]
+			});
+			addressChart.render();
 		}
-		</script> 
-
-	<!-- end med graphhh -->
-
-	
-	<!-- start address graphhh -->
-
-	<script>
-     window.onload = function () {
-      
-     var chart = new CanvasJS.Chart("chartContainer1", {
-         animationEnabled: true,
-         theme: "light3", // "light1", "light2", "dark1", "dark2"
-         title: {
-             text: "Record"
-         },
-         axisY: {
-             title: "Quantity Request"
-         },
-         axisX: {
-             title: "Address"
-         },
-         data: [{
-             type: "column",
-             dataPoints: <?php echo json_encode($test, JSON_NUMERIC_CHECK); ?>
-         }]
-     });
-     chart.render();
-      
-     }
-     </script>  
-	 <!-- end address graph -->
-
-	<!--  graph design -->
-	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-	<div id="chartContainer1" style="height: 370px; width: 100%;"></div>
-	<script src="../js/canvasjs.min.js"></script>
-
+	</script>
 </body>
-	
 </html>
